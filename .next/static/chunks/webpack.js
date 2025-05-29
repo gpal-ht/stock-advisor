49e96eb19d1cb0202815ff44bb407cdadff4dc23
Gaurav Pal <gpal-ht@users.noreply.github.com>
2025-05-28 17:03:20 -0700

    Add 'use client' directive to enable client-side features

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4def2c98e3232be8"; }
+/******/ 		__webpack_require__.h = function() { return "d9b7785ff90ea5a8"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
